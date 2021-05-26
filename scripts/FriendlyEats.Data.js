a795aa6760e2c7094ba60703cfba37d5601c8da4
yisroelm <yisroel96@gmail.com>
2021-05-26 10:46:17 -0700

    ratings fully functional

--- scripts/FriendlyEats.Data.js
+++ scripts/FriendlyEats.Data.js
@@ -71,8 +71,25 @@ FriendlyEats.prototype.getRestaurant = function(id) {
     this.getDocumentsInQuery(query, renderer);
   };
 
-FriendlyEats.prototype.addRating = function(restaurantID, rating) {
-  /*
-    TODO: Retrieve add a rating to a restaurant
-  */
-};
+  FriendlyEats.prototype.addRating = function(restaurantID, rating) {
+    var collection = firebase.firestore().collection('restaurants');
+    var document = collection.doc(restaurantID);
+    var newRatingDocument = document.collection('ratings').doc();
+  
+    return firebase.firestore().runTransaction(function(transaction) {
+      return transaction.get(document).then(function(doc) {
+        var data = doc.data();
+  
+        var newAverage =
+            (data.numRatings * data.avgRating + rating.rating) /
+            (data.numRatings + 1);
+  
+        transaction.update(document, {
+          numRatings: data.numRatings + 1,
+          avgRating: newAverage
+        });
+        return transaction.set(newRatingDocument, rating);
+      });
+    });
+  };
+  
